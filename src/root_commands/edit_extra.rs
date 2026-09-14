3d09772754b78b4ee8c18d782ca2687c0bfb4c35
haukzero <haukzero1@gmail.com>
2026-09-14 19:46:02 +0800

    feat(edit-extra): add default template for extra targets
    When using `edit-extra`, if the required config file is not detected in the current environment, a default template example is provided within the newly created config file.

--- src/root_commands/edit_extra.rs
+++ src/root_commands/edit_extra.rs
@@ -5,6 +5,18 @@ use crate::editor;
 use crate::error::Result;
 use crate::targets::EXTRA_TARGET_FILE;
 
+const DEFAULT_TEMPLATE: &[u8] = br#"# Example for extra targets
+# [[example.resources]]
+# key = "settings" # optional, default: filename
+# filename = "settings.json" # REQUIRED
+# active_path = ".example/settings.json" # REQUIRED
+# template = "{
+#     \"env\": {},
+#     \"name\": \"Example\",
+# }" # optional, default: empty
+# required = true # optional, default: true
+"#;
+
 pub fn run(editor_name: Option<String>) -> Result<()> {
     let editor_cmd = editor::resolve_editor(editor_name)?;
     let repository = config::repository_dir()?;
@@ -13,6 +25,7 @@ pub fn run(editor_name: Option<String>) -> Result<()> {
     fs::create_dir_all(repository)?;
     if !path.exists() {
         fs::File::create(&path)?;
+        fs::write(&path, DEFAULT_TEMPLATE)?;
     }
     editor::open_editor(&editor_cmd, &path)
 }
